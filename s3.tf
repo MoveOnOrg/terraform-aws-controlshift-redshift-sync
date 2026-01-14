96401a9a246b4f2f9cb048559f98eab012f902d8
Ankur Naik <ankur.naik@moveon.org>
2026-01-13 17:09:07 -0800

    Reorder sections to more closely match Controlshift's version.

--- s3.tf
+++ s3.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "manifest" {
   provider = aws.controlshift
-  bucket   = var.manifest_bucket_name
+  bucket = var.manifest_bucket_name
 
   tags = {
     Name = "ControlShift puts import manifests here"
@@ -16,37 +16,20 @@ resource "aws_s3_bucket" "manifest" {
 # Ownership controls block is required to support ACLs.
 resource "aws_s3_bucket_ownership_controls" "manifest" {
   provider = aws.controlshift
-  bucket   = aws_s3_bucket.manifest.id
+  bucket = aws_s3_bucket.manifest.id
   rule {
     object_ownership = "ObjectWriter"
   }
 }
 
-resource "aws_s3_bucket_acl" "manifest" {
-  provider   = aws.controlshift
-  depends_on = [aws_s3_bucket_ownership_controls.manifest]
-
-  bucket = aws_s3_bucket.manifest.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "manifest" {
-  provider = aws.controlshift
-  bucket   = aws_s3_bucket.manifest.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
 resource "aws_s3_bucket_lifecycle_configuration" "manifest" {
   provider = aws.controlshift
-  bucket   = aws_s3_bucket.manifest.id
+  bucket = aws_s3_bucket.manifest.id
 
+  # expire the ingested manifests after 5 days after they have been processed to save disk space while providing enough
+  # time to analyze things that might have gone wrong.
   rule {
-    id     = "expire-manifests"
+    id = "expire-manifests"
     status = "Enabled"
 
     expiration {
@@ -55,5 +38,24 @@ resource "aws_s3_bucket_lifecycle_configuration" "manifest" {
 
     # Best practice: filter is now required inside the rule block
     filter {}
+  }
+}
+
+resource "aws_s3_bucket_acl" "manifest" {
+  provider = aws.controlshift
+  depends_on = [aws_s3_bucket_ownership_controls.manifest]
+
+  bucket = aws_s3_bucket.manifest.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "manifest" {
+  provider = aws.controlshift
+  bucket = aws_s3_bucket.manifest.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
